aa98db0cbe7fabb88c65d831e25d40c3ced0b321
taozhang <2359173906@qq.com>
2020-11-28 01:12:58 +0800

    change dependences

--- src/python_gui/tkinter_test/__init__.py
+++
@@ -1,6 +0,0 @@
-import time
-import logging
-import src as project
-
-logging.info(
-    f">>> import {__file__} at time {time.asctime(time.localtime(time.time()))} for project pos {project.projectDir}")
