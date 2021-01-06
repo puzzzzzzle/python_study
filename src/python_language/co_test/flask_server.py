706a5c3e5a50b9fa05a0d5242eec8abcb92d8230
khalidzhang <khalidzhang@tencent.com>
2021-01-06 13:51:11 +0800

    add flask server study

--- src/python_language/co_test/flask_server.py
+++ src/python_language/co_test/flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
 import time
 import logging
 import consts
@@ -7,18 +7,51 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+# 符合直觉的: 参数优先级比页面低, 贪心匹配方式
+# path/ 访问地址
+# path
+# @app.route('/<name>')
+# def hello_world(name):
+#     time.sleep(2)
+#     return f'name {name}!'
+
 
 @app.route('/')
 def index():
-    time.sleep(2)
-    logger.info("get one")
-    return 'Hello! index'
+    return 'Index Page'
 
 
-@app.route('/<name>')
-def hello_world(name):
-    time.sleep(2)
-    return f'Hello {name}!'
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 
 if __name__ == '__main__':
