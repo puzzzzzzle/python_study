7b3c23fd12b2d2619bbbe4d684047f48f79ca9fb
khalidzhang <khalidzhang@tencent.com>
2021-06-18 18:56:38 +0800

    add

---
+++ src/matplot_test/matplot_template.py
@@ -0,0 +1,30 @@
+import time
+import numpy as np
+import cv2
+
+import matplotlib.pyplot as plt
+
+
+def draw_lines():
+    fig, ax = plt.subplots()  # type:plt.Figure,plt.Axes
+    ax.axis("equal")
+    ax.set_title(f"test {time.asctime()}")
+    x = [4, 9, 8, 7, 3]
+    y = [4, 1, 4, 4, 3]
+    ax.plot(x, y, "-")
+    ax.plot(x, y, "o")
+
+
+draw_lines()
+
+
+def draw_math():
+    fig, ax = plt.subplots()  # type:plt.Figure,plt.Axes
+    ax.set_title(f"draw_math {time.asctime()}")
+    x = np.arange(-100, 100)
+    y = x**3
+    ax.plot(x, y, "o")
+
+
+draw_math()
+plt.show()
